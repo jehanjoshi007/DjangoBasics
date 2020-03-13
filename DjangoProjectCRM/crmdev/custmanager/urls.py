92e7e19cdc11b4cc4075f82944e6657648139e49
jehanjoshi <[jehandjoshi@gmail.com]>
2020-03-13 10:45:08 -0400

    Django application added new features

--- DjangoProjectCRM/crmdev/custmanager/urls.py
+++ DjangoProjectCRM/crmdev/custmanager/urls.py
@@ -4,7 +4,7 @@ from . import views
 #Here the '' indicates base url redirects to Home
 
 urlpatterns = [
-    path('', views.Home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.Home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
 ]
--- DjangoProjectCRM/crmdev/custmanager/views.py
+++ DjangoProjectCRM/crmdev/custmanager/views.py
@@ -17,14 +17,20 @@ def Home(request):
     total_orders = Order.objects.values('ponumber').order_by().annotate(Count('ponumber',distinct=True))
 #    order_sum = total_orders.aggregate(sum())
 #    count_orders = total_orders.count()
+    line_pending = orders.filter(status='To Be Planned').count()
+    line_planned = orders.filter(status='Planned').count()
     total_customers = customers.count()
     context = {'orders':orders,'customers':customers,'cust_orders':cust_orders,
-    'total_orders':total_orders}
+    'total_orders':total_orders,'line_pending':line_pending,'line_planned':line_planned}
     return render(request, 'custmanager/dashboard.html', context)
 
 def products(request):
     products = Product.objects.all()
     return render(request, 'custmanager/products.html',{'products':products})
 
-def customer(request):
-    return render(request, 'custmanager/customer.html')
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    po_count = Order.objects.values('ponumber').order_by().annotate(Count('ponumber',distinct=True))
+    context = {'customer':customer,'orders':orders,'po_count':po_count}
+    return render(request, 'custmanager/customer.html',context)
