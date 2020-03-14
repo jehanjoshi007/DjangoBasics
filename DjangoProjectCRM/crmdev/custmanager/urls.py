12114edc1902d402031c8153b134f92c0b9b8970
jehanjoshi <[jehandjoshi@gmail.com]>
2020-03-14 19:32:59 -0400

    adding user authentication and restriction

--- DjangoProjectCRM/crmdev/custmanager/urls.py
+++ DjangoProjectCRM/crmdev/custmanager/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('customer/<str:pk>/', views.customer, name="customer"),
     path('create_order/<str:pk>',views.createOrder, name="create_order"),
     path('update_order/<str:pk>',views.updateOrder, name="update_order"),
-    path('delete_order/<str:pk>',views.deleteOrder, name="delete_order")
+    path('delete_order/<str:pk>',views.deleteOrder, name="delete_order"),
+    path('register/', views.register, name="register"),
+    path('loginpage/', views.loginpage, name="login"),
+    path('logoutUser/', views.logoutUser, name="logout")
 
 ]
--- DjangoProjectCRM/crmdev/custmanager/views.py
+++ DjangoProjectCRM/crmdev/custmanager/views.py
@@ -5,6 +5,11 @@ from .models import *
 from django.db.models import Count
 from .forms import *
 from .filters import *
+from django.core.paginator import Paginator
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 #Always remember this folder structure to render templates
@@ -13,6 +18,53 @@ from .filters import *
 #------appname
 #--------template.html
 
+def register(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account was created for'  +  user)
+                return redirect('login')
+        context={'form':form}
+        return render(request,'custmanager/register.html',context)
+
+def loginpage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+
+            user = authenticate(request,username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+                return render(request,'custmanager/login.html')
+
+        context = {}
+        return render(request,'custmanager/login.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def Home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -27,10 +79,12 @@ def Home(request):
     'total_orders':total_orders,'line_pending':line_pending,'line_planned':line_planned}
     return render(request, 'custmanager/dashboard.html', context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request, 'custmanager/products.html',{'products':products})
 
+@login_required(login_url='login')
 def customer(request,pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
@@ -40,6 +94,7 @@ def customer(request,pk):
     context = {'customer':customer,'orders':orders,'po_count':po_count,'my_filter':my_filter}
     return render(request, 'custmanager/customer.html',context)
 
+@login_required(login_url='login')
 def createOrder(request,pk):
     OrderFormSet = inlineformset_factory(Customer,Order,fields=('ponumber','customer','linenumber','product','price','QuantityY2','QuantityRolls','status'),extra=5)
     customer = Customer.objects.get(id=pk)
@@ -56,6 +111,8 @@ def createOrder(request,pk):
     context = {'formset':formset}
     return render(request, 'custmanager/order_form.html',context)
 
+
+@login_required(login_url='login')
 def updateOrder(request,pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -68,6 +125,8 @@ def updateOrder(request,pk):
     context = {'form':form}
     return render(request, 'custmanager/order_form.html',context)
 
+
+@login_required(login_url='login')
 def deleteOrder(request,pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
@@ -75,3 +134,22 @@ def deleteOrder(request,pk):
         return redirect('/')
     context = {'item':order}
     return render(request,'custmanager/delete.html',context)
+
+# def listing(request):
+#     order_list = Order.objects.select_related('customer','product').order_by('ponumber','linenumber').all()
+#     paginator = Paginator(order_list,2)
+#     page_request_var = "page"
+#     page = request.GET.get(page_request_var)
+#     try:
+#         queryset = paginator.page(page)
+#     except PageNotAnInteger:
+#         queryset = paginator.page(1)
+#     except EmptyPage:
+#         queryset = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         "object_list":queryset,
+#         "title":"list",
+#         "page_request_var":page_request_var,
+#         "order_list":order_list}
+#     return render(request, 'custmanager/dashboard.html', context)
